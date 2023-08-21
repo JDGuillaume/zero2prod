576dc4024be57513a3657fa17f3a66e07b100ccb
Joey <josephdguillaume@gmail.com>
2023-08-21 06:57:17 -0400

    Renamed variable in run.

--- src/startup.rs
+++ src/startup.rs
@@ -7,13 +7,13 @@ use std::net::TcpListener;
 use tracing_actix_web::TracingLogger;
 
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
-    let connection = web::Data::new(db_pool);
+    let db_pool = web::Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
             .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
-            .app_data(connection.clone())
+            .app_data(db_pool.clone())
     })
     .listen(listener)?
     .run();
