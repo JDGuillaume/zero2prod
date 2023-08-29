4c3ecd1f5d734b2f6ef76f8567209a5aa4bbbcc4
Joey <josephdguillaume@gmail.com>
2023-08-29 06:43:35 -0400

    Added support for confirmation links including functionality, tests, and database actions.

--- src/configuration.rs
+++ src/configuration.rs
@@ -41,7 +41,10 @@ pub struct ApplicationSettings {
     #[serde(deserialize_with = "deserialize_number_from_string")]
     pub port: u16,
     pub host: String,
+    pub base_url: String,
 }
+
+pub struct ApplicationBaseUrl(pub String);
 
 #[derive(Clone, serde::Deserialize)]
 pub struct Settings {
